8908545ac1e1845b5d5d491d66853d8afbfd26fe
thruy <huuhoang.3002@gmail.com>
2026-04-19 22:56:01 +0700

    set layout

---
+++ frontend/src/features/products/pages/ProductDetail.js
@@ -0,0 +1,7 @@
+function ProductDetail() {
+    return (
+        <h1>Trang hiển thị thông tin chi tiết sản phẩm</h1>
+    )
+}
+
+export default ProductDetail;
---
+++ frontend/src/features/products/pages/Products.js
@@ -0,0 +1,7 @@
+function Products() {
+    return (
+        <h1>Trang hiển thị danh sách sản phẩm</h1>
+    )
+}
+
+export default Products;

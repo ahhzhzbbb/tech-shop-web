0d383b5329ebc72ae2e4921b1c2cf4ebf4d5ae55
ahhzhzbbb <ahhzhzbbb@gmail.com>
2026-04-23 21:02:28 +0700

    them tai khoan mac dinh

--- backend/src/main/java/com/example/shop/DataInitializer.java
+++ backend/src/main/java/com/example/shop/DataInitializer.java
@@ -1,14 +1,35 @@
 package com.example.shop;
 
+import com.example.shop.models.AppRole;
+import com.example.shop.models.Role;
+import com.example.shop.models.User;
+import com.example.shop.repositories.RoleRepository;
+import com.example.shop.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public void run(String... args) {
 
+        Role role = new Role();
+        role.setRoleName(AppRole.ROLE_ADMIN);
+
+        roleRepository.save(role);
+
+        User newUser = new User();
+        newUser.setUsername("admin");
+        newUser.setPassword(passwordEncoder.encode("admin"));
+        newUser.setPhoneNumber("0585424988");
+        newUser.setRole(role);
+        userRepository.save(newUser);
     }
 }

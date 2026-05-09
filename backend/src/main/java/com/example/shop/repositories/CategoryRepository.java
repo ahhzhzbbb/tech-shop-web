002ef927de2cc7c4dfbebfc913c53ed715facb48
Hà Huy Hoàng <hoang.hh235335@sis.hust.edu.vn>
2026-05-09 11:15:17 +0700

    them category repository

---
+++ backend/src/main/java/com/example/shop/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.example.shop.repositories;
+
+import com.example.shop.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    boolean existsByNameIgnoreCase(String name);
+    Optional<Category> findByNameIgnoreCase(String name);
+    
+}

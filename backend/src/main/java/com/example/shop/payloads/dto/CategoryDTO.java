7da6161358c26402cb9f2f158580ea1b5ea48cff
Hà Huy Hoàng <hoang.hh235335@sis.hust.edu.vn>
2026-05-09 13:39:17 +0700

    Update CategoryDTO.java

--- backend/src/main/java/com/example/shop/payloads/dto/CategoryDTO.java
+++ backend/src/main/java/com/example/shop/payloads/dto/CategoryDTO.java
@@ -1,6 +1,5 @@
 package com.example.shop.payloads.dto;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +9,8 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class CategoryDTO {
+
     private Long id;
     private String name;
-    private Boolean active;
 }

53b2d8c671de2febbb7398d7a99c7d799f63455e
Hà Huy Hoàng <hoang.hh235335@sis.hust.edu.vn>
2026-05-09 11:19:55 +0700

    Update CategoryService.java

--- backend/src/main/java/com/example/shop/services/CategoryService.java
+++ backend/src/main/java/com/example/shop/services/CategoryService.java
@@ -1,1 +1,11 @@
+package com.example.shop.services;
+import com.example.shop.models.Category;
+import java.util.List;
 
+public interface CategoryService {
+    List<Category> getAllCategory();
+    Category getCategoryAndProduct(Long id);
+    Category createCategory(Category category);
+    Category updateCategory(Long id, Category category);
+    void deleteCategory(Long id);
+}

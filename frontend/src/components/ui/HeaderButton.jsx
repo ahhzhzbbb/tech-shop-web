0f9bc8a262bf951fe18d60375f4f29cffb50c101
hoang-cloudy <truongviethoang949@gmail.com>
2026-05-23 17:13:02 +0700

    Sua header lan 2

--- frontend/src/components/ui/HeaderButton.jsx
+++ frontend/src/components/ui/HeaderButton.jsx
@@ -4,6 +4,7 @@ import "./HeaderButton.scss";
 import { useNavigate } from "react-router-dom";
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 const HeaderButton = ({ icon, title, subtitle, to, onClick, variant = "transparent", ...rest }) => {
   const navigate = useNavigate();
   const handleClick = () => {
@@ -46,6 +47,32 @@ const HeaderButton = ({ icon, title, subtitle, to, onClick }) => {
       </div>
     </Button>
   );
+=======
+const HeaderButton = ({ icon, title, subtitle, to, onClick, variant = "transparent" }) => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        if (onClick) {
+            onClick();
+            return;
+        }
+        if (to) {
+            navigate(to);
+        }
+    };
+
+    return (
+        <Button className={`header-button variant-${variant}`} onClick={handleClick}>
+            <div className="hb-icon">
+                {icon}
+            </div>
+
+            <div className="hb-content">
+                <span className="hb-title">{title}</span>
+                {subtitle && <span className="hb-subtitle">{subtitle}</span>}
+            </div>
+        </Button>
+    );
+>>>>>>> 5ea5351 (Sua header lan 2)
 };
 
 export default HeaderButton;

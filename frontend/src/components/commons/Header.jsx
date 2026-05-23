8f2d9ca41bdbc8357009c7533bb3910672cbc533
hoang-cloudy <truongviethoang949@gmail.com>
2026-05-23 17:13:02 +0700

    Sua header lan 2

--- frontend/src/components/commons/Header.jsx
+++ frontend/src/components/commons/Header.jsx
@@ -22,7 +22,7 @@ import HeaderButton from "../ui/HeaderButton.jsx";
 import "./Header.scss";
 <<<<<<< HEAD
 import SearchInput from "../../features/search/components/SearchInput.js"
-import { ShoppingCartOutlined, ContainerOutlined, EnvironmentOutlined, UserAddOutlined, LoginOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons';
+import { ShoppingCartOutlined, ContainerOutlined, EnvironmentOutlined, UserOutlined, MenuOutlined, PhoneOutlined, LogoutOutlined } from '@ant-design/icons';
 import LoginModal from "../../features/auth/component/LoginModal.jsx";
 import Dropdown from "../ui/Dropdown.jsx";
 import { Button, Popconfirm, message } from "antd";
@@ -85,29 +85,34 @@ const Header = () => {
     return (
         <>
             <div className="header">
-                <div className="header_left">
-                    <img src={logo} className="header_logo" />
-                    <img src={shopName} className="header_logo" />
-                </div>
+                <div className="header_container">
+                    <div className="header_left">
+                        <div style={{ display: 'flex', gap: '4px', cursor: 'pointer' }} onClick={() => navigate('/')}>
+                            <img src={logo} className="header_logo" alt="Logo" />
+                            <img src={shopName} className="header_logo" alt="Shop Name" />
+                        </div>
+                        <HeaderButton icon={<MenuOutlined />} title="Danh mục" subtitle="" variant="dark" />
+                    </div>
 
-                <div className="header_center">
-                    <SearchInput />
-                </div>
+                    <div className="header_center">
+                        <SearchInput />
+                    </div>
 
-                <div className="header_right">
-                    <HeaderButton icon={<EnvironmentOutlined />} title="Hệ thống" subtitle="cửa hàng" />
-                    <HeaderButton icon={<ContainerOutlined />} title="Tìm" subtitle="đơn hàng" to="/orders" />
-                    <HeaderButton icon={<ShoppingCartOutlined />} title="Giỏ hàng" subtitle="của bạn" to="/cart" />
-                    {user ? (
-                        <>
-                            <Dropdown icon={<UserOutlined />} title="Xin chào," subtitle={user.username} menu={{ items }} />
-                        </>
-                    ) : (
-                        <>
-                            <HeaderButton icon={<UserAddOutlined />} title="Đăng ký" onClick={() => setOpenRegister(true)} />
-                            <HeaderButton icon={<LoginOutlined />} title="Đăng nhập" onClick={() => setOpenLogin(true)} />
-                        </>
-                    )}
+                    <div className="header_right">
+                        <HeaderButton icon={<PhoneOutlined />} title="Hotline" subtitle="1900.5301" />
+                        <HeaderButton icon={<EnvironmentOutlined />} title="Hệ thống" subtitle="Showroom" />
+                        <HeaderButton icon={<ContainerOutlined />} title="Tra cứu" subtitle="đơn hàng" to="/orders" />
+                        <HeaderButton icon={<ShoppingCartOutlined />} title="Giỏ" subtitle="hàng" to="/cart" />
+                        {user ? (
+                            <>
+                                <Dropdown icon={<UserOutlined />} title="Tài khoản" subtitle={user.username} menu={{ items }} variant="dark" />
+                            </>
+                        ) : (
+                            <>
+                                <HeaderButton icon={<UserOutlined />} title="Đăng nhập" subtitle="" variant="dark" onClick={() => setOpenLogin(true)} />
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
 

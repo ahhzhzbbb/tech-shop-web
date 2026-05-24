c594732c4c22c3e5494fc717fe1658b01702fde5
hoang-cloudy <truongviethoang949@gmail.com>
2026-05-24 16:12:53 +0700

    add ProductGallery

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -6,11 +6,18 @@ import ProductDetail from "../features/products/pages/ProductDetail.jsx";
 import UserInfo from "../features/user/pages/UserInfo.jsx";
 import Cart from "../features/cart/pages/Cart.jsx";
 import Orders from "../features/order/pages/Orders.jsx"
+import AdminLayout from "../layouts/AdminLayout.jsx";
+import AdminDashboard from "../features/admin/dashboard/pages/AdminDashboard.jsx";
+import AdminOrders from "../features/admin/orders/AdminOrders.jsx"
+import AdminCategories from "../features/admin/category/AdminCategories.jsx"
+import AdminProducts from "../features/admin/products/AdminProducts.jsx"
+import AdminStatistics from "../features/admin/statistics/AdminStatistics.jsx"
 
 const AppRoute = () => {
     return (
         <BrowserRouter>
             <Routes>
+                {/* user */}
                 <Route element={<MainLayout />}>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/products/:category" element={<Products />} />
@@ -18,6 +25,15 @@ const AppRoute = () => {
                     <Route path="/user" element={<UserInfo />} />
                     <Route path="/cart" element={<Cart />} />
                     <Route path="/orders" element={<Orders />} />
+                </Route>
+
+                {/* admin */}
+                <Route element={<AdminLayout />}>
+                    <Route path="/admin" element={<AdminDashboard />} />
+                    <Route path="/admin/products" element={<AdminProducts />} />
+                    <Route path="/admin/categories" element={<AdminCategories />} />
+                    <Route path="/admin/orders" element={<AdminOrders />} />
+                    <Route path="/admin/statistics" element={<AdminStatistics />} />
                 </Route>
             </Routes>
         </BrowserRouter>

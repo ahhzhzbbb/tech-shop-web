5cfb276b141e5cfa30e11577d0cdf4b4f061f7db
thruy <huuhoang.3002@gmail.com>
2026-05-30 00:37:57 +0700

    merge conflict

--- frontend/src/features/auth/services/auth.service.jsx
+++ frontend/src/features/auth/services/auth.service.jsx
@@ -1,19 +1,18 @@
-import axios from "axios";
-const AUTH_URL = "http://localhost:8080/api/auth";
+import api from '../../../../services/api';
 
 <<<<<<< HEAD
 export const registerApi = async (data) => {
-    const response = await axios.post(`${AUTH_URL}/signup`, data, { withCredentials: true });
+    const response = await api.post('/api/auth/signup', data, { withCredentials: true });
     return response.data;
 }
 
 export const loginApi = async (data) => {
-    const response = await axios.post(`${AUTH_URL}/login`, data, { withCredentials: true });
+    const response = await api.post('/api/auth/login', data, { withCredentials: true });
     return response.data;
 }
 
 export const logoutApi = async () => {
-    const response = await axios.post(`${AUTH_URL}/signout`, {}, { withCredentials: true });
+    const response = await api.post('/api/auth/signout', {}, { withCredentials: true });
     return response.data;
 }
 =======

76e3de40612567d34dbc4dcfd1343ef21dcb1349
thruy <huuhoang.3002@gmail.com>
2026-05-30 01:01:05 +0700

    fix useauth conflict

--- frontend/src/features/auth/hooks/useAuth.jsx
+++ frontend/src/features/auth/hooks/useAuth.jsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
-<<<<<<< HEAD
 import { registerApi, loginApi, logoutApi } from "../services/auth.service";
 import { useAuthContext } from "../../../context/AuthContext.jsx";
-=======
-import { registerApi, loginApi } from "../services/auth.service";
->>>>>>> 087dbf7 (sua UI header)
 
 export const useRegister = () => {
   const [loading, setLoading] = useState(false);
@@ -40,29 +36,29 @@ export const useLogin = () => {
   };
 
 <<<<<<< HEAD
-    return { login, loading };
+  return { login, loading };
 }
 
 export const useLogout = () => {
-    const [loading, setLoading] = useState(false);
-    const { setUser } = useAuthContext();
+  const [loading, setLoading] = useState(false);
+  const { setUser } = useAuthContext();
 
-    const logout = async () => {
-        try {
-            setLoading(true);
-            await logoutApi();
-            setUser(null);
-            // Xóa token từ localStorage nếu có
-            localStorage.removeItem('token');
-            return true;
-        } catch (err) {
-            throw err?.response?.data || err;
-        } finally {
-            setLoading(false);
-        }
-    };
+  const logout = async () => {
+    try {
+      setLoading(true);
+      await logoutApi();
+      setUser(null);
+      // Xóa token từ localStorage nếu có
+      localStorage.removeItem('token');
+      return true;
+    } catch (err) {
+      throw err?.response?.data || err;
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return { logout, loading };
+  return { logout, loading };
 }
 =======
   return { login, loading };

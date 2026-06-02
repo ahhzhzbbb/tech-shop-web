f646509483fc4614d6e22e46fedb154ee75a0ff4
Hoanghang <140964461+Zekom-Pi@users.noreply.github.com>
2026-06-02 09:51:26 +0700

    Update CartItem.java

--- backend/src/main/java/com/example/shop/models/CartItem.java
+++ backend/src/main/java/com/example/shop/models/CartItem.java
@@ -1,39 +1,29 @@
 package com.example.shop.models;
-import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.security.Timestamp;
+import lombok.*;
 
 @Entity
-@Table(name = "cart_items", uniqueConstraints = {
-    @UniqueConstraint(columnNames = {"cart_id", "product_id"})
-})
+@Table(name = "cart_items")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class CartItem {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private Integer quantity;
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Timestamp createdAt;
-    private Timestamp updatedAt;
 
-    @ManyToOne(fetch = FetchType.LAZY , optional = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
-    @ManyToOne(fetch = FetchType.LAZY , optional = false)
-    @JoinColumn(name = "cart_id", nullable = false)
-    @JsonBackReference
-    private Cart cart;
 }
